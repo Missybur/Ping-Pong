4595570246cd105a52a77978d12b213d737d5d71
Missy Bur <missybur@gmail.com>
2017-09-24 21:07:54 -0400

    Add test cases for mocha

--- test/main_test.js
+++ test/main_test.js
@@ -1,6 +1,10 @@
-
+var expect = require('chai').expect;
 
 // Test suite
 describe(' ', function() {
+  // Test spec (unit test)
+  it('should run our tests using npm', function() {
+    expect(true).to.be.ok;
+  })
 
 });
